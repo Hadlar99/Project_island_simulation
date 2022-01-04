85ebd8841b6f090ddd076aa400f68b6d3da94fbd
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-04 14:10:30 +0100

    added lists of animals

--- biosim_template/src/biosim/Landscape.py
+++ biosim_template/src/biosim/Landscape.py
@@ -1,7 +1,9 @@
 class lowland:
 
-    def __init__(self):
+    def __init__(self, herbivores=None, carnivores= None):
         self.fodder = 800
+        self.herbivores = herbivores if herbivores is not None else []
+        self.carnivores = carnivores if carnivores is not None else []
 
     def feasting (self, N):
 
