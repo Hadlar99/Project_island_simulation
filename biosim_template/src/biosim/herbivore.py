aa64ff27a586017ade526b7353bb335f537728e7
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-04 12:08:00 +0100

    made a function year

--- biosim_template/src/biosim/herbivore.py
+++ biosim_template/src/biosim/herbivore.py
@@ -1,7 +1,15 @@
+import math as m
+
+
 class herbavore:
 
-    def __init__(self, w_birth, a_half, w_half, phi_weight, phi_age):
+    def __init__(self, w_birth, a_half, w_half, phi_weight, phi_age, beta, eta):
         self.age = 0
-        self.w_birth = w_birth
+        self.weight = w_birth
         self.fitness = fitness
+
+    def year(self, F):
+        self.age += 1
+        self.weight = F*beta - self.weight*eta
+        self.fitness = 1/(1 + m.exp(phi_age(self.age-a_half))) * 1/(1 + m.exp(phi_weight(w_half-self.weight))) if self.weight>0 else 0
 
