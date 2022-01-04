08e67f9bf4bc01e34a92a1899dbcb44a7d5bbd6c
Harald Norvald Stabbetorp <harald.norvald.stabbetorp@nmbu.no>
2022-01-04 12:24:39 +0100

    put all the variabels in the init function

--- biosim_template/src/biosim/herbivore.py
+++ biosim_template/src/biosim/herbivore.py
@@ -1,15 +1,21 @@
 import math as m
 
 
-class herbavore:
+class Herbivore:
 
     def __init__(self, w_birth, a_half, w_half, phi_weight, phi_age, beta, eta):
         self.age = 0
         self.weight = w_birth
-        self.fitness = fitness
+        self.a_half = a_half
+        self. w_half = w_half
+        self.phi_weight = phi_weight
+        self.phi_age = phi_age
+        self.beta = beta
+        self.eta = eta
+
 
     def year(self, F):
         self.age += 1
-        self.weight = F*beta - self.weight*eta
-        self.fitness = 1/(1 + m.exp(phi_age(self.age-a_half))) * 1/(1 + m.exp(phi_weight(w_half-self.weight))) if self.weight>0 else 0
+        self.weight = F*self.beta - self.weight*self.eta
+        self.fitness = 1/(1 + m.exp(self.phi_age(self.age-self.a_half))) * 1/(1 + m.exp(self.phi_weight(self.w_half-self.weight))) if self.weight>0 else 0
 
