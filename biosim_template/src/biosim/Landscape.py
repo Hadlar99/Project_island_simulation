b1d3cd9fd5f96a2a7f91381c855075d220726180
Harald Norvald Stabbetorp <harald.norvald.stabbetorp@nmbu.no>
2022-01-04 14:27:14 +0100

    made a for loop to decide how much each herbivore are going to eat

--- biosim_template/src/biosim/Landscape.py
+++ biosim_template/src/biosim/Landscape.py
@@ -3,7 +3,11 @@ class lowland:
     def __init__(self, herbivores=None, carnivores= None):
         self.fodder = 800
         self.herbivores = herbivores if herbivores is not None else []
+        self.herbivores = sorted (self.herbivores, key=lambda x: x.fitness, reverse=True)
         self.carnivores = carnivores if carnivores is not None else []
 
-    def feasting (self, N):
+    def feeding (self):
+        for i in self.herbivores:
+            if self.fodder >= 10:
+                self.fodder -= 10
 
