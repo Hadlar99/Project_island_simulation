772897889b31d8180cad1072a07689d58ab2256d
Harald Norvald Stabbetorp <harald.norvald.stabbetorp@nmbu.no>
2022-01-05 15:48:01 +0100

    made a test for the funcion year

---
+++ biosim_template/tests/Test_Herbivore.py
@@ -0,0 +1,9 @@
+from .src.biosim.herbivore import Herbivore
+import pytest
+
+def test_age():
+    num_year = 21
+    herbivore = Herbivore(8)
+    for i in range (num_year):
+        herbivore.year(10)
+    assert herbivore.age == num_year
