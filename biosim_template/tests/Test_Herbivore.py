f51c37c5a2ae2c97990eec9c5d956dd4611166d3
Harald Norvald Stabbetorp <harald.norvald.stabbetorp@nmbu.no>
2022-01-05 20:38:28 +0100

    made tests to check weight

--- biosim_template/tests/Test_Herbivore.py
+++ biosim_template/tests/Test_Herbivore.py
@@ -3,9 +3,21 @@ import pytest
 
 def test_age():
     num_year = 21
-    herbivore = Herbivore(8)
+    herbivore = Herbivore()
     for i in range (num_year):
         herbivore.year()
     assert herbivore.age == num_year
 
 
+def test_add_weight():
+    herbivore = Herbivore()
+    herbivore.add_weight(10)
+
+    assert herbivore.weight >= 0
+
+def test_lose_weight():
+    herbivore = Herbivore()
+    previous_weight = herbivore.weight
+    herbivore.lose_weight()
+
+    assert herbivore.weight < previous_weight
