b208fb3edbcfba6dc0fc1e68db5381082120a627
Harald Norvald Stabbetorp <harald.norvald.stabbetorp@nmbu.no>
2022-01-05 11:49:39 +0100

    changed the way we choose the appetite of herbivores

--- biosim_template/src/biosim/Landscape.py
+++ biosim_template/src/biosim/Landscape.py
@@ -1,17 +1,22 @@
 class lowland:
 
-    def __init__(self, herbivores=None, carnivores= None):
+    def __init__(self, F_herbivores, herbivores=None, carnivores= None):
         self.fodder = 800
         self.herbivores = herbivores if herbivores is not None else []
         self.herbivores = sorted(self.herbivores, key=lambda x: x.fitness, reverse=True)
         self.carnivores = carnivores if carnivores is not None else []
+        self.F_herbivores = F_herbivores
 
     def feeding(self):
         for herbi in self.herbivores:
-            if self.fodder >= 10:
-                self.fodder -= 10
-                herbi.year(10)
+            if self.fodder >= self.F_herbivores:
+                self.fodder -= self.F_herbivores
+                herbi.year(self.F_herbivores)
             else:
                 herbi.year(self.fooder)
                 self.fooder = 0
+
+    def reproduction(self):
+
+
 
