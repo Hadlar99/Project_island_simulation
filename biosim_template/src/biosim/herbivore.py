578dfe8eff70daafdb5b12bcbc587335a4351caa
Harald Norvald Stabbetorp <harald.norvald.stabbetorp@nmbu.no>
2022-01-05 13:00:51 +0100

    fixed parameter issue

--- biosim_template/src/biosim/herbivore.py
+++ biosim_template/src/biosim/herbivore.py
@@ -39,28 +39,29 @@ class Herbivore:
     @classmethod
     def set_params(cls, params):
 
+        pass
 
 
 
 
-    def __init__(self, wieght):
+    def __init__(self, weight):
         self.age = 0
         self.weight = weight
-        self.fitness = 1/(1 + m.exp(phi_age(self.age-a_half))) * 1/(1 + m.exp(phi_weight(w_half-self.weight))) if self.weight>0 else 0
+        self.fitness = 1/(1 + m.exp(self.phi_age(self.age-self.a_half))) * 1/(1 + m.exp(self.phi_weight(self.w_half-self.weight))) if self.weight>0 else 0
 
 
 
     def year(self, F):
         self.age += 1
-        self.weight = F*beta - self.weight*eta
-        self.fitness = 1/(1 + m.exp(phi_age(self.age-a_half))) * 1/(1 + m.exp(phi_weight(w_half-self.weight))) if self.weight>0 else 0
+        self.weight = F*self.beta - self.weight*self.eta
+        self.fitness = 1/(1 + m.exp(self.phi_age(self.age-self.a_half))) * 1/(1 + m.exp(self.phi_weight(self.w_half-self.weight))) if self.weight>0 else 0
 
     def birth(self, N):
-        if self.weight < zeta * (w_birth + sigma_birth):
+        if self.weight < self.zeta * (self.w_birth + self.sigma_birth):
             return False
-        elif random.uniform(0, 1) < min(1, gamma*self.fitness*(N-1)):
-            weight_baby = random.gauss(w_birth, sigma_birth)
-            self.weight -= xi * weight_baby
+        elif random.uniform(0, 1) < min(1, self.gamma*self.fitness*(N-1)):
+            weight_baby = random.gauss(self.w_birth, self.sigma_birth)
+            self.weight -= self.xi * weight_baby
             return weight_baby
         else:
             return False
@@ -68,7 +69,7 @@ class Herbivore:
     def death(self):
         if self.weight == 0:
             return True
-        elif random.uniform(0,1) < omega *(1-self.fitness):
+        elif random.uniform(0,1) < self.omega *(1-self.fitness):
             return True
         else:
             return False
