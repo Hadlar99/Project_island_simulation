72e47672da68606ea817a7b28c55ae3a4185aeb0
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-05 12:13:36 +0100

    set default params in herbivore

--- biosim_template/src/biosim/herbivore.py
+++ biosim_template/src/biosim/herbivore.py
@@ -3,20 +3,54 @@ import math as m
 
 class Herbivore:
 
+    w_birth = 8.0
+    sigma_birth = 1.5
+    beta = 0.9
+    eta = 0.05
+    a_half = 40.0
+    phi_age = 0.6
+    w_half = 10.0
+    phi_weight = 0.1
+    mu = 0.25
+    gamma = 0.2
+    zeta = 3.5
+    xi = 1.2
+    omega = 0.4
+    F = 10.0
 
-    def __init__(self, w_birth, a_half, w_half, phi_weight, phi_age, beta, eta):
+    default_params = {'w_birth': w_birth,
+                    'sigma_birth': sigma_birth,
+                    'beta': beta,
+                    'eta': eta,
+                    'a_half': a_half,
+                    'phi_age': phi_age,
+                    'w_half': w_half,
+                    'phi_weight': phi_weight,
+                    'mu': mu,
+                    'gamma': gamma,
+                    'zeta': zeta,
+                    'xi': xi,
+                    'omega': omega,
+                    'F': F}
+
+
+
+    @classmethod
+    def set_params(cls, params):
+
+
+
+
+
+    def __init__(self):
         self.age = 0
         self.weight = w_birth
-        self.a_half = a_half
-        self. w_half = w_half
-        self.phi_weight = phi_weight
-        self.phi_age = phi_age
-        self.beta = beta
-        self.eta = eta
+        self.fitness = 1/(1 + m.exp(phi_age(self.age-a_half))) * 1/(1 + m.exp(phi_weight(w_half-self.weight))) if self.weight>0 else 0
+
 
 
     def year(self, F):
         self.age += 1
-        self.weight = F*self.beta - self.weight*self.eta
-        self.fitness = 1/(1 + m.exp(self.phi_age(self.age-self.a_half))) * 1/(1 + m.exp(self.phi_weight(self.w_half-self.weight))) if self.weight>0 else 0
+        self.weight = F*beta - self.weight*eta
+        self.fitness = 1/(1 + m.exp(phi_age(self.age-a_half))) * 1/(1 + m.exp(phi_weight(w_half-self.weight))) if self.weight>0 else 0
 
