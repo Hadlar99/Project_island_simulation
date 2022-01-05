53cad2d9572d723aad931dc4ae745dcf8c109a74
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-05 11:32:20 +0100

    added to the feeding function in lowland

--- biosim_template/src/biosim/Landscape.py
+++ biosim_template/src/biosim/Landscape.py
@@ -3,11 +3,15 @@ class lowland:
     def __init__(self, herbivores=None, carnivores= None):
         self.fodder = 800
         self.herbivores = herbivores if herbivores is not None else []
-        self.herbivores = sorted (self.herbivores, key=lambda x: x.fitness, reverse=True)
+        self.herbivores = sorted(self.herbivores, key=lambda x: x.fitness, reverse=True)
         self.carnivores = carnivores if carnivores is not None else []
 
-    def feeding (self):
-        for i in self.herbivores:
+    def feeding(self):
+        for herbi in self.herbivores:
             if self.fodder >= 10:
                 self.fodder -= 10
+                herbi.year(10)
+            else:
+                herbi.year(self.fooder)
+                self.fooder = 0
 
