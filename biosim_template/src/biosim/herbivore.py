2ae8c4515c70e26ef684810a1a7c720ce9367f12
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-05 15:25:51 +0100

    made set_params function in Herbivore

--- biosim_template/src/biosim/herbivore.py
+++ biosim_template/src/biosim/herbivore.py
@@ -4,42 +4,33 @@ import random
 
 class Herbivore:
 
-    w_birth = 8.0
-    sigma_birth = 1.5
-    beta = 0.9
-    eta = 0.05
-    a_half = 40.0
-    phi_age = 0.6
-    w_half = 10.0
-    phi_weight = 0.1
-    mu = 0.25
-    gamma = 0.2
-    zeta = 3.5
-    xi = 1.2
-    omega = 0.4
-    F = 10.0
 
-    default_params = {'w_birth': w_birth,
-                    'sigma_birth': sigma_birth,
-                    'beta': beta,
-                    'eta': eta,
-                    'a_half': a_half,
-                    'phi_age': phi_age,
-                    'w_half': w_half,
-                    'phi_weight': phi_weight,
-                    'mu': mu,
-                    'gamma': gamma,
-                    'zeta': zeta,
-                    'xi': xi,
-                    'omega': omega,
-                    'F': F}
+    params = {w_birth : 8.0,
+        sigma_birth : 1.5,
+        beta : 0.9,
+        eta : 0.05,
+        a_half : 40.0,
+        phi_age : 0.6,
+        w_half : 10.0,
+        phi_weight : 0.1,
+        mu : 0.25,
+        gamma : 0.2,
+        zeta : 3.5,
+        xi : 1.2,
+        omega : 0.4,
+        F : 10.0}
 
 
 
     @classmethod
-    def set_params(cls, params):
+    def set_params(cls, given_params):
+    for key in given_params:
+        if key not in params:
+            raise KeyError(f'Invalid parameter name: {key}')
 
-        pass
+    for key in given_params():
+        params[key] = given_params[key]
+
 
 
 
