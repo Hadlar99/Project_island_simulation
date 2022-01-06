f225b40a976f1140107d1aff263dbb4fa709ed8f
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-06 17:57:27 +0100

    added docstrings to Landscape.py and Test_lowland.py

--- biosim_template/tests/Test_lowland.py
+++ biosim_template/tests/Test_lowland.py
@@ -1,16 +1,21 @@
+"""Test for lowland class"""
+import random
+
 from biosim.Landscape import lowland
 from biosim.herbivore import Herbivore
 
 import pytest
-
+seed = 456
 
 def test_count_herbivores():
+    """Tests the if the count_herbivores count correctly"""
     cell = lowland([Herbivore() for _ in range(50)])
 
     assert cell.num_herbivores() == 50
 
 
 def test_feeding():
+    """Tests if the right amount of food are eaten"""
     cell = lowland([Herbivore() for _ in range(50)])
     cell.feeding()
 
@@ -18,6 +23,7 @@ def test_feeding():
 
 
 def test_feeding_no_fodder():
+    """Test if there are no food left when all the food are eaten"""
     cell = lowland([Herbivore() for _ in range(90)])
     cell.feeding()
 
@@ -25,6 +31,8 @@ def test_feeding_no_fodder():
 
 
 def test_reproduction():
+    """Test if there are getting more herbivores when new herbivores are born"""
+    random.seed(seed)
     cell = lowland([Herbivore(3, 35) for _ in range(10)])
     cell.reproduction()
 
@@ -32,20 +40,25 @@ def test_reproduction():
 
 
 def test_aging():
+    """Tests if the herbivores are aging correctly"""
     cell = lowland([Herbivore(a, 35) for a in range(10)])
     cell.aging()
 
-    assert cell.herbivores[-1].age == 10
+    assert all(herbi.age == i + 1 for i, herbi in enumerate(cell.herbivores))
 
 
 def test_loss_of_weight():
+    """Test if the herbivores loses weight correctly """
     cell = lowland([Herbivore(7, 35)])
     cell.loss_of_weight()
 
     assert cell.herbivores[0].weight == 35 - 35 * cell.herbivores[0].params['eta']
 
+
 def test_pop_reduction():
-    cell = lowland([Herbivore(a, 35) for a in range(50)])
+    """Test if there are getting less herbivores when herbivores dies """
+    random.seed(seed)
+    cell = lowland([Herbivore(a, 20) for a in range(50)])
     cell.pop_reduction()
 
     assert cell.num_herbivores() < 50
