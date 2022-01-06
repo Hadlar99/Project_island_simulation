4d64ca6418529fe87a5a9c0cd79753d4b082cb67
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-06 16:35:14 +0100

    made test_aging and test_loss_of_weight

--- biosim_template/tests/Test_lowland.py
+++ biosim_template/tests/Test_lowland.py
@@ -3,6 +3,7 @@ from biosim.herbivore import Herbivore
 
 import pytest
 
+
 def test_count_herbivores():
     cell = lowland([Herbivore() for _ in range(50)])
 
@@ -15,14 +16,30 @@ def test_feeding():
 
     assert cell.fodder == 800-50*10
 
+
 def test_feeding_no_fodder():
     cell = lowland([Herbivore() for _ in range(90)])
     cell.feeding()
 
     assert cell.fodder == 0
 
+
 def test_reproduction():
     cell = lowland([Herbivore(3, 35) for _ in range(10)])
     cell.reproduction()
 
-    assert cell.num_herbivores() < 10
+    assert cell.num_herbivores() > 10
+
+
+def test_aging():
+    cell = lowland([Herbivore(a, 35) for a in range(10)])
+    cell.aging()
+
+    assert cell.herbivores[-1].age == 10
+
+
+def test_loss_of_weight():
+    cell = lowland([Herbivore(7, 35)])
+    cell.loss_of_weight()
+
+    assert cell.herbivores[0].weight == 35 - 35 * cell.herbivores[0].params['eta']
