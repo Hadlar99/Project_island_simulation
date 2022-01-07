0e1dcbf94a6d6cbb45a85fb191bff9d2f3a1413f
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-07 18:44:45 +0100

    changed some docstrings

--- biosim_template/src/biosim/herbivore.py
+++ biosim_template/src/biosim/herbivore.py
@@ -3,24 +3,25 @@ import random
 
 """This is a class for a single Herbivore"""
 
+
 class Herbivore:
 
     """Given parameters that works with the code"""
 
     params = {'w_birth': 8.0,
-        'sigma_birth': 1.5,
-        'beta': 0.9,
-        'eta': 0.05,
-        'a_half': 40.0,
-        'phi_age': 0.6,
-        'w_half': 10.0,
-        'phi_weight': 0.1,
-        'mu': 0.25,
-        'gamma': 0.2,
-        'zeta': 3.5,
-        'xi': 1.2,
-        'omega': 0.4,
-        'F': 10.0}
+              'sigma_birth': 1.5,
+              'beta': 0.9,
+              'eta': 0.05,
+              'a_half': 40.0,
+              'phi_age': 0.6,
+              'w_half': 10.0,
+              'phi_weight': 0.1,
+              'mu': 0.25,
+              'gamma': 0.2,
+              'zeta': 3.5,
+              'xi': 1.2,
+              'omega': 0.4,
+              'F': 10.0}
 
     """Method to change parameter when given a dictionary with same keys"""
     @classmethod
@@ -75,7 +76,7 @@ class Herbivore:
         -------
 
         """
-        if self.weight <= 0: #if the herbivore weight is less than 0 it cannot get any fitness
+        if self.weight <= 0:    # if the herbivore weight is less than 0 it cannot get any fitness
             return 0
         return 1 / (1 + m.exp(self.params['phi_age'] * (self.age - self.params['a_half']))) * 1 / (
                     1 + m.exp(self.params['phi_weight'] * (self.params['w_half'] - self.weight)))
@@ -93,14 +94,15 @@ class Herbivore:
 
         """
         if self.weight < self.params['zeta'] * (self.params['w_birth'] + self.params['sigma_birth']):
-            return False #if the baby is going to weigh more thatn the parent, no birth
+            return False    # if the mother weighs too little, no birth
         elif random.random() < min(1, self.params['gamma']*self.fitness()*(N-1)):
-            weight_baby = random.gauss(self.params['w_birth'], self.params['sigma_birth']) #gives a weight to baby if birth
+            weight_baby = random.gauss(self.params['w_birth'], self.params['sigma_birth'])
+            # gives a weight to baby if birth
             if weight_baby > self.weight:
-                return False  #if the baby is going to weigh more thatn the parent, no birth
+                return False  # if the baby is going to weigh more than the parent, no birth
             if weight_baby <= 0:
-                return False  # Baby not born if it weight is less or equal to 0
-            self.weight -= self.params['xi'] * weight_baby # reduce weight of parent when given birth
+                return False  # baby not born if it weight is less or equal to 0
+            self.weight -= self.params['xi'] * weight_baby  # reduce weight of parent when given birth
             return weight_baby
         else:
             return False
@@ -108,8 +110,8 @@ class Herbivore:
     def death(self):
         """Sets conditions for an animal to die"""
         if self.weight == 0:
-            return True #if the weight is 0 it's going to die
+            return True     # if the weight is 0 it's going to die
         elif random.random() < self.params['omega'] * (1-self.fitness()):
-            return True #if less fit, more likely to die
+            return True     # if less fit, more likely to die
         else:
-            return False #if not dead, it's going to live
+            return False       # if not dead, it's going to live
