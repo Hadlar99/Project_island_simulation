5a15d26097ae477885a3d7a64f814c0f9c2ac696
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-09 14:54:38 +0100

    added docstrings and a couple of tests to Test_island

--- biosim_template/tests/Test_island.py
+++ biosim_template/tests/Test_island.py
@@ -1,5 +1,9 @@
 from biosim.Island import Island
 import textwrap
+import random
+
+seed = 1234
+
 geogr = """\
                WWW
                WLW
@@ -11,14 +15,36 @@ ini_herbs = [{'loc': (2, 2),
                        'weight': 20}
                       for _ in range(50)]}]
 
+ini_carns = [{'loc': (2, 2),
+              'pop': [{'species': 'Carnivore',
+                       'age': 5,
+                       'weight': 20}
+                      for _ in range(20)]}]
 
-def test_feeding():
+
+def test_amount_of_herbivores():
+    """Test if the function amount_of_herbivores counts the amount of carnivores correctly"""
+    world = Island(geogr, ini_herbs)
+    assert world.amount_of_herbivores() == 50
+
+
+def test_amount_carnivores():
+    """Test if the function amount_of_carnivores counts the amount of carnivores correctly"""
+    world = Island(geogr, ini_carns)
+    assert world.amount_of_carnivores() == 20
+
+
+def test_new_animals():
+    """Tests if the new_animal function works correctly"""
+    cell = Island(geogr, ini_herbs)
+    cell.new_animals(ini_herbs)
+    assert cell.amount_of_herbivores() == 100
+
+
+def test_season():
+    """Tests if there are born more herbivore"""
+    random.seed(seed)
     cell = Island(geogr, ini_herbs)
     cell.season()
     cell.season()
     assert cell.amount_of_herbivores() > 50
-
-
-def test_amount_of_herbivores():
-    world = Island(geogr, ini_herbs)
-    assert world.amount_of_herbivores() == 50
