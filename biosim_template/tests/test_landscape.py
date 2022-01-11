76afa09ca48ad67f71a29362a59e521383a225b0
Jon Markus Borgundvåg Berg <jon.markus.borgundvag.berg@nmbu.no>
2022-01-11 13:46:57 +0100

    updated the tests for landscape

--- biosim_template/tests/test_landscape.py
+++ biosim_template/tests/test_landscape.py
@@ -5,6 +5,14 @@ from biosim.landscape import Lowland, Highland, Dessert, Water
 from biosim.animal import Herbivore, Carnivore
 import pytest
 seed = 456
+ini_herbs = [{'species': 'Herbivore',
+              'age': 5,
+              'weight': 20}
+             for _ in range(50)]
+ini_carns = [{'species': 'Carnivore',
+              'age': 5,
+              'weight': 20}
+             for _ in range(20)]
 
 
 def test_count_herbivores():
@@ -27,14 +35,15 @@ def test_pop_animals():
                       for _ in range(50)])
     assert cell.num_herbivores(), cell.num_carnivores() == 50
 
+
 def test_pop_animals_ValueError():
     """Checks if the program raises a ValueError if wrong species"""
     with pytest.raises(ValueError):
         cell = Lowland()
         cell.pop_animals([{'species': 'Animal',
-                       'age': 5,
-                       'weight': 20}
-                      for _ in range(50)])
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(50)])
 
 
 def test_feeding():
@@ -43,6 +52,7 @@ def test_feeding():
     cell.feeding()
 
     assert cell.fodder == 800-50*10
+
 
 def test_feeding_if_no_fodder():
     """Test when all the fodder goes empty"""
@@ -55,14 +65,7 @@ def test_feeding_if_no_fodder():
 def test_feeding_carnivores():
     """Test if the carnivores are going to eat any herbivores"""
     random.seed(seed)
-    ini_herbs = [{'species': 'Herbivore',
-                  'age': 5,
-                  'weight': 20}
-                 for _ in range(50)]
-    ini_carns = [{'species': 'Carnivore',
-                  'age': 5,
-                  'weight': 20}
-                 for _ in range(20)]
+
     cell = Lowland()
     cell.pop_animals(ini_herbs)
     cell.pop_animals(ini_carns)
@@ -70,6 +73,7 @@ def test_feeding_carnivores():
     cell.carnivore_feeding()
 
     assert cell.num_herbivores() < 50
+
 
 def test_reproduction():
     """Test if there are getting more herbivores when new herbivores are born"""
@@ -83,7 +87,7 @@ def test_reproduction():
 def test_aging():
     """Tests if the herbivores are aging correctly"""
     cell = Lowland([Herbivore(a, 35) for a in range(10)], [Carnivore(a, 35) for a in range(10)])
-    cell.aging()
+    cell.aging_and_loss_of_weight()
 
     assert all(herbi.age == i + 1 for i, herbi in enumerate(cell.herbivores))
 
@@ -91,7 +95,7 @@ def test_aging():
 def test_loss_of_weight():
     """Test if the herbivores loses weight correctly """
     cell = Lowland([Herbivore(7, 35)])
-    cell.loss_of_weight()
+    cell.aging_and_loss_of_weight()
 
     assert cell.herbivores[0].weight == 35 - 35 * cell.herbivores[0].params['eta']
 
@@ -112,15 +116,18 @@ def test_food_params():
     cell.feeding()
     assert cell.fodder == 0
 
+
 def test_moving_params():
     """Test if is possible to move to this landscape"""
     cell = Lowland()
     assert cell.move
 
+
 def test_moving_water_params():
     """Test if it is not possible to move to this landscape"""
     cell = Water()
     assert not cell.move
+
 
 def test_migration():
     """Tests if the function migration returns 2 lists"""
@@ -128,4 +135,3 @@ def test_migration():
     cell_herbi, cell_carni = cell.migration()
 
     assert type(cell_herbi) == list and type(cell_carni) == list
-
